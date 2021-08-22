0474dbdffb02ef84e0623fcec37c5f4bbe1f88e6
selenium.merida <selenium.merida@gmail.com>
2021-08-21 23:21:38 -0500

    Added java demo application files

---
+++ src/main/java/Main.java
@@ -0,0 +1,13 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	public static boolean checkIfInputIsAnEvenNumber(int number){
+        return number % 2 == 0;
+    }
+
+}
